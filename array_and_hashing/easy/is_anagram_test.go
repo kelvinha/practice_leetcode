0ab270af4ece89323f0181111435e3a938ec7123
kelvinha <kelvinhartanto18@gmail.com>
2025-02-20 20:47:47 +0700

    [easy] is anagram - in progress

--- array_and_hashing/easy/is_anagram_test.go
+++ array_and_hashing/easy/is_anagram_test.go
@@ -23,18 +23,27 @@ func validationAnagram(s, t string) bool {
 	if len(runesAnagram1) == len(runesAnagram2) {
 		var isValid []bool
 		anagramMaps := map[rune]rune{}
+		duplicateCharRunesAnagram1 := map[rune]int{}
+		duplicateCharRunesAnagram2 := map[rune]int{}
 
 		// save all text to map
 		for i := 0; i < len(runesAnagram1); i++ {
+			if anagramMaps[runesAnagram1[i]] == runesAnagram1[i] {
+				duplicateCharRunesAnagram1[runesAnagram1[i]] = duplicateCharRunesAnagram1[runesAnagram1[i]] + 1
+			}
+
 			anagramMaps[runesAnagram1[i]] = runesAnagram1[i]
 		}
 
 		// validate if runes from string 2 having runes in map from string 1
 		for i := 0; i < len(runesAnagram2); i++ {
 			if anagramMaps[runesAnagram2[i]] == runesAnagram2[i] {
+				duplicateCharRunesAnagram2[runesAnagram2[i]] = duplicateCharRunesAnagram2[runesAnagram2[i]] + 1
 				isValid = append(isValid, true)
 			}
 		}
+
+		log.Println("test: ", duplicateCharRunesAnagram1)
 
 		if len(isValid) == len(t) {
 			return true
