0d94f29a0f8cf4673ccd04bc810cb20f5b992983
kelvin hartanto <kelvinhartanto18@gmail.com>
2025-02-21 11:02:45 +0700

    [easy] is anagram - done

--- array_and_hashing/easy/is_anagram_test.go
+++ array_and_hashing/easy/is_anagram_test.go
@@ -10,46 +10,38 @@ func TestAnagram(t *testing.T) {
 	//log.Println(validationAnagram("anagram", "nagaram"))
 }
 
-// anagram => angrm => 5
-// nagaram => nagrm => 5
-
-// aacc => ac => 2
-// ccac => ca => 2
-
 func validationAnagram(s, t string) bool {
 	runesAnagram1 := []rune(s)
 	runesAnagram2 := []rune(t)
 
 	if len(runesAnagram1) == len(runesAnagram2) {
-		var isValid []bool
 		anagramMaps := map[rune]rune{}
 		duplicateCharRunesAnagram1 := map[rune]int{}
 		duplicateCharRunesAnagram2 := map[rune]int{}
 
 		// save all text to map
 		for i := 0; i < len(runesAnagram1); i++ {
+			anagramMaps[runesAnagram1[i]] = runesAnagram1[i]
 			if anagramMaps[runesAnagram1[i]] == runesAnagram1[i] {
 				duplicateCharRunesAnagram1[runesAnagram1[i]] = duplicateCharRunesAnagram1[runesAnagram1[i]] + 1
 			}
-
-			anagramMaps[runesAnagram1[i]] = runesAnagram1[i]
 		}
 
 		// validate if runes from string 2 having runes in map from string 1
 		for i := 0; i < len(runesAnagram2); i++ {
 			if anagramMaps[runesAnagram2[i]] == runesAnagram2[i] {
 				duplicateCharRunesAnagram2[runesAnagram2[i]] = duplicateCharRunesAnagram2[runesAnagram2[i]] + 1
-				isValid = append(isValid, true)
 			}
 		}
 
-		log.Println("test: ", duplicateCharRunesAnagram1)
-
-		if len(isValid) == len(t) {
-			return true
+		// validate if each character from anagram2 is available in anagram1 or not
+		for _, v := range anagramMaps {
+			if duplicateCharRunesAnagram1[v] != duplicateCharRunesAnagram2[v] {
+				return false
+			}
 		}
 
-		return false
+		return true
 	}
 
 	return false
