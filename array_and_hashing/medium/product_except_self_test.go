e828e4a83f581367fa42b54c1b06120b106ed58e
kelvin hartanto <kelvinhartanto18@gmail.com>
2025-04-16 15:07:54 +0700

    [medium] product except self - done

--- array_and_hashing/medium/product_except_self_test.go
+++ array_and_hashing/medium/product_except_self_test.go
@@ -1,24 +1,47 @@
 package medium
 
 import (
+	"log"
 	"testing"
 )
 
 func TestProductExceptSelf(t *testing.T) {
-	productExceptSelf([]int{1, 2, 3, 4})
+	log.Println("V1", productExceptSelf([]int{1, 2, 3, 4}))
+	log.Println("V2", productExceptSelfV2([]int{1, 2, 3, 4}))
 }
 
 func productExceptSelf(nums []int) []int {
 	var finalResult []int
-	for i := 0; i < len(nums); i++ {
-		result := 1
-		for j := 0; j < len(nums); j++ {
+	var result int
+	total := len(nums)
+	for i := 0; i < total; i++ {
+		result = 1
+		for j := 0; j < total; j++ {
 			if i == j {
 				continue
 			}
-			result = result * nums[j]
+			result *= nums[j]
 		}
 		finalResult = append(finalResult, result)
 	}
 	return finalResult
+}
+
+func productExceptSelfV2(nums []int) []int {
+	n := len(nums)
+	result := make([]int, n)
+
+	prefix := 1
+	for i := 0; i < n; i++ {
+		result[i] = prefix
+		prefix *= nums[i]
+	}
+
+	suffix := 1
+	for i := n - 1; i >= 0; i-- {
+		result[i] *= suffix
+		suffix *= nums[i]
+	}
+
+	return result
 }
